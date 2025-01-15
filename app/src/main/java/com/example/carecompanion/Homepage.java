5613ff946edcbbecc033066c9b752ae08b4c9587
Amina <aminahaq05@hotmail.com>
2025-01-15 15:47:45 +0000

    clean up the code and made smalled changes for better navigation

--- app/src/main/java/com/example/carecompanion/Homepage.java
+++ app/src/main/java/com/example/carecompanion/Homepage.java
@@ -1,20 +1,31 @@
 package com.example.carecompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
+    }
 
+    // Method to handle the launch of the Settings page
+    public void launchSettingsPage(View view) {
+        // Create an Intent to open the SettingsActivity
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
+    public void launchMedicalIDPage(View view) {
+        // Create an intent to launch the Medical ID page
+        Intent intent = new Intent(this, Medical_Id.class);
+        startActivity(intent);
     }
 }
+
+
--- app/src/main/java/com/example/carecompanion/Medical_Id.java
+++ app/src/main/java/com/example/carecompanion/Medical_Id.java
@@ -1,12 +1,10 @@
 package com.example.carecompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Medical_Id extends AppCompatActivity {
 
@@ -15,6 +13,17 @@ public class Medical_Id extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_medical_id);
+    }
 
+    // Method for back button to navigate to Homepage
+    public void launchHomepage(View view) {
+        Intent intent = new Intent(Medical_Id.this, Homepage.class);
+        startActivity(intent);
+    }
+
+    // Method for settings button to navigate to Settings Activity
+    public void launchSettingsPage(View view) {
+        Intent intent = new Intent(Medical_Id.this, Settings.class);
+        startActivity(intent);
     }
 }
